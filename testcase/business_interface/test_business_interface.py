301ed00d68b7a8e23257d6c1c994fd5719d9e034
Bao Zhu <ice.michael.zhu@gmail.com>
2026-02-17 16:02:36 +0200

    add m_id , c_id

--- testcase/business_interface/test_business_interface.py
+++ testcase/business_interface/test_business_interface.py
@@ -3,13 +3,14 @@ import pytest
 from pathlib import Path
 
 from base.api_util_business import RequestBaseBusiness
+from base.generateId import m_id, c_id
 from common.read_yaml import ReadYaml
 
 BASE_DIR = Path(__file__).resolve().parent
 
-
+@allure.feature(next(m_id) + '电子商务管理系统（业务场景）')
 class TestBusinessInterface:
-    @allure.story('商品列表到下单支付流程')
+    @allure.story(next(c_id) + '商品列表到下单支付流程')
     @pytest.mark.parametrize("case_info", ReadYaml.read_test_case_yaml(BASE_DIR / "businessCase.yaml"))
     def test_business_interface(self, case_info):
         allure.dynamic.title(case_info['baseInfo']["api_name"])
--- testcase/interface/test_login.py
+++ testcase/interface/test_login.py
@@ -4,13 +4,14 @@ import allure
 import pytest
 
 from base.api_util import RequestBase
+from base.generateId import m_id, c_id
 from common.read_yaml import ReadYaml
 
 TESTCASE_DIR = Path(__file__).parent
-
+@allure.feature(next(m_id)+ "测试登录模块")
 class TestLogin:
 
-    @allure.story("查询用户")
+    @allure.story(next(c_id)+"查询用户")
     @pytest.mark.parametrize("base_info,test_case", ReadYaml.read_test_case_yaml(TESTCASE_DIR / "queryUser.yaml"))
     def test_query_user(self,base_info, test_case):
         allure.dynamic.title(test_case['case_name'])
